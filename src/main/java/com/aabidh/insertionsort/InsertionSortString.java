03b05d0b973bfebd1d4677267ad9aa8ad64424ad
Axdcodes <aabidhme@gmail.com>
2022-05-05 19:32:47 +0530

    initial commit

---
+++ src/main/java/com/aabidh/insertionsort/InsertionSortString.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.aabidh.insertionsort;
+
+/**
+ *
+ * @author Home
+ */
+public class InsertionSortString {
+
+    public void Insertionsort(String array[]) {
+        int n = array.length;
+        String temp;
+
+        for (int x = 1; x < n; x++) {
+            int key = array[x];
+            int y = x - 1;
+
+                array[y + 1] = array[y];
+                y = y - 1;
+
+            }
+            array[y + 1] = key;
+        }
+
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        InsertionSortTest obj = new InsertionSortTest();
+        String myarray[] = {"brandon","mike","alex"};
+        System.out.println("Array elements before sorting: ");
+
+        for (int i = 0; i < myarray.length; i++) {
+            System.out.print(myarray[i] + " ");
+        }
+
+        obj.Insertionsort(myarray);
+        System.out.println("\nArray elements after sorting: ");
+
+        for (int i = 0; i < myarray.length; i++) {
+            System.out.print(myarray[i] + " ");
+        }
+    }
+}
